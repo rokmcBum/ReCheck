4e450ac18e921212b7c249858f28bd566de4c507
Heheheesu <yhs9459@naver.com>
2025-06-13 02:56:44 +0900

    google-services.json 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.ksp)
-    alias(libs.plugins.googleServices)
 }
 
 android {
--- app/src/main/java/com/example/recheck/MainActivity.kt
+++ app/src/main/java/com/example/recheck/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             ReCheckTheme {
                 RecheckApp()
-                // CalendarScreen(currentUserId = 1)
             }
         }
     }
