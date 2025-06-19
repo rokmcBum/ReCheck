0ba722a2f3bc891aa82ef2fc961d47233ce25479
bumkyung.seo <peter4024@naver.com>
2025-06-19 12:50:53 +0900

    style: init screen

--- app/src/main/java/com/example/recheck/uicomponent/InitScreen.kt
+++ app/src/main/java/com/example/recheck/uicomponent/InitScreen.kt
@@ -1,22 +1,31 @@
 package com.example.recheck.uicomponent
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.recheck.R
 import com.example.recheck.model.Routes
@@ -24,28 +33,59 @@ import com.example.recheck.model.Routes
 @Composable
 fun InitScreen(navController: NavController) {
     Surface(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xFFFFFFFF))
+        ) {
             Column(
-                modifier = Modifier.align(Alignment.Center),
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 300.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.logo),
                     contentDescription = "App Logo",
-                    modifier = Modifier.size(160.dp)
+                    modifier = Modifier
+                        .size(160.dp)
                 )
+
             }
 
-            Button(
-                onClick = { navController.navigate(Routes.Login.route) },
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(24.dp)
-                    .height(60.dp)
                     .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp) // 하단 여백
+                    .width(358.dp)
+                    .height(48.dp)
+                    .background(
+                        color = Color(0xFFFF5D5D),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .clickable { navController.navigate(Routes.Login.route) }
             ) {
-                Text("로그인", fontWeight = FontWeight.Bold)
+                Button(
+                    onClick = { navController.navigate(Routes.Login.route) },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.White
+                    ),
+                    elevation = null
+                ) {
+                    Text(
+                        text = "시작하기",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Medium
+                        ),
+                        color = Color.White
+                    )
+                }
             }
         }
     }
 }
+
