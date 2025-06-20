78887d80bb30f7c75bfc729c1316eee9ca44009a
Heheheesu <yhs9459@naver.com>
2025-06-20 09:06:02 +0900

    폰트 적용

--- app/src/main/java/com/example/recheck/ui/theme/Type.kt
+++ app/src/main/java/com/example/recheck/ui/theme/Type.kt
@@ -1,19 +1,36 @@
 package com.example.recheck.ui.theme
 
+import android.R.attr.fontFamily
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.recheck.R
+
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight  = FontWeight.SemiBold,
+        fontSize    = 22.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Pretendard,
+        fontWeight  = FontWeight.Normal,
+        fontSize    = 16.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight  = FontWeight.Medium,
+        fontSize    = 11.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
